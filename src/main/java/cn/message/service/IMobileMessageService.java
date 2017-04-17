a1485795ee802b0eb7a13c7c2a0b3221d3dbe8be
gaoxigang <gaoxigang@chudaokeji.com>
2017-04-17 17:43:17 +0800

    增加注释

--- src/main/java/cn/message/service/IMobileMessageService.java
+++ src/main/java/cn/message/service/IMobileMessageService.java
@@ -3,6 +3,9 @@ package cn.message.service;
 public interface IMobileMessageService {
 	/**
 	 * 发送手机短信消息
+	 * @param mobile 手机号码
+	 * @param content 发送内容
+	 * @return
 	 */
 	boolean sendMessage(String mobile,String content);
 }
